d41528fb0dbe0c76e1f851a10059acf27b7a84dd
TRX <trx@TRXs-MacBook-Air.local>
2023-07-17 12:58:11 +0330

    try to write the loop of the printer but I need a printer it's enough for now

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 import argon2
 import json
-
+import os
 
 def check_username_password(username, password):
     # hash the username and password
@@ -20,6 +20,19 @@ def check_username_password(username, password):
         return True, pulbic_id
     except:
         return False, None
+
+
+def pdf_files(location):
+    '''
+        this function is used to get the pdf files from the given location
+        the function returns a list of pdf files
+    '''
+    files = os.listdir(location)
+    pdf_files = []
+    for file in files:
+        if file.endswith('.pdf'):
+            pdf_files.append(os.path.join(location, file))
+    return pdf_files
 
 
 
